36bc2001c68ce731dae769a7ddcde8905e381e01
Shivu Gururaj <gururajshivu@gmail.com>
2016-03-18 14:54:41 -0500

    button actions

---
+++ tapButton/tapButton/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tapButton
+//
+//  Created by Shivu Gururaj on 3/17/16.
+//  Copyright © 2016 shivugururaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLable: UITextView!
+    
+    @IBAction func buttonPress(sender: UIButton) {
+        
+    }
+    
+
+}
+
